8b394c18d20c05c388670b7cf4cce65abfc57c2a
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-12-18 16:59:01 +0000

    aula-6

---
+++ aula-6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula06
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("===================Verificador de Número Par ou Ímpar====================");
+      Console.WriteLine("Digite um número: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+
+      int result = number % 2;
+      Console.WriteLine("===================Resultado====================");
+      if(result == 0)
+      {
+        Console.WriteLine("Esse número é Par!");
+      }
+      else
+      {
+        Console.WriteLine("Esse número é Ímpar!");
+      }
+    }
+  }
+}
+
