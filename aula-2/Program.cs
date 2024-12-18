17d77545f6b70dce50f1dff448ccc5e36bb15235
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-12-18 08:32:22 +0000

    first commit

---
+++ aula-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula02;
+
+public class Program
+{
+  public static void Main()
+  {
+     string myName = "Fred";
+     int age = 20;
+     double height = 1.65;
+     Console.WriteLine($"Oi meu nome é {myName} e tenho {age} anos e tenho {height} de altura.");
+  }
+}
