b67030cf921b805eca4089ced28fc158f022dc12
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-12-18 10:13:19 +0000

    aula-4

---
+++ aula-4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula04;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("-----CADASTRO DE USUÁRIO------");
+
+    // Solicitar o nome do Usuário
+    Console.WriteLine("Digite seu Nome:");
+    string name = Console.ReadLine();
+   
+    // Solicitar a Idade
+    Console.WriteLine("Digite sua Idade:");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    // Solicitar a Idade
+    Console.WriteLine("Digite sua Altura:");
+    double height = Convert.ToDouble(Console.ReadLine());
+
+    // Resultado
+    Console.WriteLine($"-----------------------");
+    Console.WriteLine($"Nome do Usuário: {name}");
+    Console.WriteLine($"Idade: {age}");
+    Console.WriteLine($"Altura: {height} metros");
+  }
+}
