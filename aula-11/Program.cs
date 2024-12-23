0fd461b71d9777ed606090f0f59ca3e9cc8d7471
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-12-23 09:12:27 +0000

    aula-11

---
+++ aula-11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula11;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite um número:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    switch (number)
+    {
+      case 1:
+        Console.WriteLine("O número é 1.");
+        break;
+      case 2:
+        Console.WriteLine("O número é 2.");
+        break;
+      default: // O default no switch funciona como um "else" e é executado se nenhum dos outros casos for verdadeiro.
+        Console.WriteLine("O número é outro qualquer.");
+        break;
+    }
+  }
+}
--- aula-11/obj/Debug/net9.0/aula-11.AssemblyInfo.cs
+++ aula-11/obj/Debug/net9.0/aula-11.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("aula-10")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("aula-11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae011c2c39115f171ca3fd0e47de7b6c512f7b5")]
-[assembly: System.Reflection.AssemblyProductAttribute("aula-10")]
-[assembly: System.Reflection.AssemblyTitleAttribute("aula-10")]
+[assembly: System.Reflection.AssemblyProductAttribute("aula-11")]
+[assembly: System.Reflection.AssemblyTitleAttribute("aula-11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
