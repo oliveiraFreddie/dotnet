00bab0650c8485516ac2685ec7b4637dd4dd9685
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-12-23 09:33:13 +0000

    aula-12

---
+++ aula-12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula12;
+
+public class Program
+{
+  public static void Main()
+  {
+    int i = 0;
+
+    while (i <= 12)
+    {
+      Console.WriteLine($"O valor de i é: {i}");
+      i++;
+      Console.WriteLine("Incrementou +1");
+    }
+  }
+}
